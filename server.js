e989c8485ea0a7aab368d19aaad4ff3f63654145
nadav-r <nadavra@gmail.com>
2019-11-23 20:56:18 +0200

    start rest api

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 8000
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'db_playground'
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+app.get('/yearsBirths', (req, res) => {
+
+    MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if (error) {
+            return console.log('Unable to connect to database')
+        }
+        const db = client.db(databaseName)
+       const agg = db.collection('users')
+            .aggregate([
+                {
+                    $group: {
+                        _id: { $substr: ["$birthday", 0, 4] },
+                        count: { $sum: 1 }
+                    }
+                }
+            ]).toArray().then(res => console.log(res))
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
