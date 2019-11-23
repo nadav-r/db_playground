f98e213f156e6615e76dac6be5ee1b1d67ca3e1c
nadav-r <nadavra@gmail.com>
2019-11-23 08:50:48 +0200

    generate dummy db

--- db_generator/db_generator.js
+++ db_generator/db_generator.js
@@ -2,19 +2,31 @@ const fs = require('fs')
 const csv = require('csv-parser')
 const generateBirthDay = require('./userGenerator/birthDayGenerator.js')
 const generateName = require('./userGenerator/nameGenerator.js')
+const uuidv1 = require('uuid/v1')
 
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: "../db.sqlite"
+    },
+    useNullAsDefault: true
+})
+
+
+// const { MongoClient, ObjectID } = require('mongodb')
+// const connectionURL = 'mongodb://127.0.0.1:27017'
+// const databaseName = '../db.sqlite'
+
+// MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+//     if (error) {
+//         return console.log('Unable to connect to database')
+//     }
+
+//     const db = client.db(databaseName)
+
+// })
 
 const results = []
-
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-
-
-
-console.log(generateBirthDay())
-
 fs.createReadStream('names-state.csv')
     .pipe(csv())
     .on('data', (data) => results.push(data))
@@ -22,7 +34,19 @@ fs.createReadStream('names-state.csv')
         const min = 0
         const max = results.length
         const names = results.map(record => { return record.name })
-        console.log(generateName(names, names))
+        const state = results.map(record => { return record.state })
+        for (let i = 0; i < 50; i++) {
+            const { firstName, secondName } = generateName(names, names)
+            const user = {
+                id: uuidv1(),
+                first_name: firstName,
+                last_name: secondName,
+                birthday: generateBirthDay(),
+                last_login: generateBirthDay()
+            }
+            knex('users').insert(user).catch(err => console.log(err))
 
-    });
+        }
+    })
+
 
--- db_generator/userGenerator/nameGenerator.js
+++ db_generator/userGenerator/nameGenerator.js
@@ -1,14 +1,11 @@
 const generateRandomWithinRange = require('../generateRandomWithinRange.js')
 
 
-module.exports =  function generateName (firstNames, secondNames) {
-    let i = parseInt(generateRandomWithinRange(0,firstNames.length))
+module.exports = function generateName(firstNames, secondNames) {
+    let i = parseInt(generateRandomWithinRange(0, firstNames.length))
     const first = firstNames[i]
-    console.log(i)
-    i = parseInt(generateRandomWithinRange(0,secondNames.length))
+    i = parseInt(generateRandomWithinRange(0, secondNames.length))
     const second = secondNames[i]
-    console.log(i)
-
     return {
         firstName: first,
         secondName: second
